b77ca3ffc64427044ac2ce549f8d9dc63703ab9f
AMBachiri <bachiriahmedmahmoud@gmail.com>
2024-07-19 16:33:52 +0100

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -3,13 +3,15 @@
 
 terraform {
 
+  /*
   cloud {
-    organization = "organization-name"
+    organization = "ambachiri"
 
     workspaces {
       name = "learn-terraform"
     }
   }
+  */
 
   required_providers {
     aws = {
